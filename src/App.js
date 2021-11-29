471bf30a8d0e27785c660cea72ce1046ef54bc95
pietow <pprotassow@posteo.de>
2021-11-29 23:48:39 +0100

    lifting

--- src/App.js
+++ src/App.js
@@ -1,30 +1,40 @@
-import  Home  from "./components/Home.js"
-import  Vocals  from "./components/Vocals.js"
-import  Guitar  from "./components/Guitar.js"
-import  Bass  from "./components/Bass.js"
-import  Drums  from "./components/Drums.js"
-import { Link, Routes, Route } from "react-router-dom";
+import { useState } from "react";
 import "./App.css";
 
-function App() {
+function Counter(props) {
   return (
-    <div className="App flex flex-col items-center bg-gray-900 text-white h-full">
-        <h1><a href="./home"><img className="filter invert" src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ffamfonts.com%2Fwp-content%2Fuploads%2Fblack-sabbath-wide.png&f=1&nofb=1" alt="" /></a></h1>
-      <div className="flex justify-between w-2/12 pt-3 pb-3">
-          <Link to="vocals">Vocals</Link>
-          <Link to="guitar">Guitar</Link>
-          <Link to="bass">Bass</Link>
-          <Link to="drums">Drums</Link>
-      </div>
-      <Routes>
-          <Route path="/" element={ <Home /> }></Route>
-          <Route path="/vocals" element={ <Vocals /> }></Route>
-          <Route path="/guitar" element={ <Guitar /> }></Route>
-          <Route path="/bass" element={ <Bass /> }></Route>
-          <Route path="/drums" element={ <Drums /> }></Route>
-          <Route path="*" element={ <Home /> }></Route>
-      </Routes>
+    <div className="border-2">
+      <h2>{props.count}</h2>
+      <p>Child1</p>
+    </div>
+  );
+}
 
+function InputField({count, setCount}) {
+  const [input, setInput] = useState(""); //my state of my child; my parent does not need to know about it; therefore I don't need to lift it up.
+
+  return (
+    <div className="border-2">
+      <input
+        className="border-2 mr-2"
+        value={input}
+        onChange={(e) => {
+          setInput(e.target.value);
+        }}
+      />
+          <button className="border-2 rounded p-2" onClick={() => setCount(parseInt(input) + count)}>+</button>
+      <p>Child2</p>
+    </div>
+  );
+}
+
+function App() {
+  const [count, setCount] = useState(1);
+  return (
+    <div>
+      <h2>Parent Component</h2>
+      <Counter count={count}></Counter>
+      <InputField count={count} setCount={setCount}></InputField>
     </div>
   );
 }
