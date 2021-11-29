3af98681c299b95e1f28df5508eb3252bb824e35
piet <pprotassow@posteo.de>
2021-11-29 15:47:29 +0100

    home link

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from "./logo.svg";
 import  Home  from "./components/Home.js"
 import  Vocals  from "./components/Vocals.js"
 import  Guitar  from "./components/Guitar.js"
@@ -10,7 +9,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App flex flex-col items-center bg-gray-900 text-white h-full">
-      <h1><img className="filter invert" src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ffamfonts.com%2Fwp-content%2Fuploads%2Fblack-sabbath-wide.png&f=1&nofb=1" alt="" /></h1>
+        <h1><a href="./home"><img className="filter invert" src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ffamfonts.com%2Fwp-content%2Fuploads%2Fblack-sabbath-wide.png&f=1&nofb=1" alt="" /></a></h1>
       <div className="flex justify-between w-2/12 pt-3 pb-3">
           <Link to="vocals">Vocals</Link>
           <Link to="guitar">Guitar</Link>
