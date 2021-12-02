3e39c43118cdd014ef2c5659ec216044ad654571
piet <pprotassow@posteo.de>
2021-12-02 11:36:34 +0100

    everything clean

--- src/App.js
+++ src/App.js
@@ -1,30 +1,9 @@
-import  Home  from "./components/Home.js"
-import  Vocals  from "./components/Vocals.js"
-import  Guitar  from "./components/Guitar.js"
-import  Bass  from "./components/Bass.js"
-import  Drums  from "./components/Drums.js"
-import { Link, Routes, Route } from "react-router-dom";
 import "./App.css";
 
 function App() {
   return (
     <div className="App flex flex-col items-center bg-gray-900 text-white h-full">
-        <h1><a href="./home"><img className="filter invert" src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ffamfonts.com%2Fwp-content%2Fuploads%2Fblack-sabbath-wide.png&f=1&nofb=1" alt="" /></a></h1>
-      <div className="flex justify-between w-2/12 pt-3 pb-3">
-          <Link to="vocals">Vocals</Link>
-          <Link to="guitar">Guitar</Link>
-          <Link to="bass">Bass</Link>
-          <Link to="drums">Drums</Link>
-      </div>
-      <Routes>
-          <Route path="/" element={ <Home /> }></Route>
-          <Route path="/vocals" element={ <Vocals /> }></Route>
-          <Route path="/guitar" element={ <Guitar /> }></Route>
-          <Route path="/bass" element={ <Bass /> }></Route>
-          <Route path="/drums" element={ <Drums /> }></Route>
-          <Route path="*" element={ <Home /> }></Route>
-      </Routes>
-
+       <p>Hello World</p>
     </div>
   );
 }
--- src/components/Drums.js
+++
@@ -1,8 +0,0 @@
-export default function Drums() {
-  return (
-    <div>
-      <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse3.mm.bing.net%2Fth%3Fid%3DOIP.r1aZM8jUQRd9FOGHYQ_LfQAAAA%26pid%3DApi&f=1" alt="" />
-    </div>
-  );
-}
-
