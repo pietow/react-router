a409e7a98de9ef48510c52b0ac984003d969b697
piet <pprotassow@posteo.de>
2021-12-02 14:38:09 +0100

    not *****working

--- src/App.js
+++ src/App.js
@@ -1,11 +1,45 @@
-import "./App.css";
+import "./App.css"
+
+import { Routes, Route } from "react-router-dom"
+import React, { useState } from "react"
+
 
 function App() {
-  return (
-    <div className="App flex flex-col items-center bg-gray-900 text-white h-full">
-       <p>Hello World</p>
-    </div>
-  );
+    const [cards, setCards] = useState([{ up: "gg" }])
+    const addCard = (key, value) => {
+        setCards([...cards, { key: value }])
+    }
+    const deleteCard = (id) => {
+        setCards([...cards.slice(0, id), ...cards.slice(id)])
+    }
+    const editCard = (id, key, value) => {
+        setCards([...cards.slice(0, id), { key: value }, ...cards.slice(id)])
+    }
+
+    return (
+        <div className="App flex flex-col items-center bg-gray-900 text-white h-full">
+            <Routes>
+                <Route path="/" element={Editor}></Route>
+                {/* <Route path="/view" element={View}></Route> */}
+                {/* <Route */}
+                {/*     path="*" */}
+                {/*     element={ */}
+                {/*         <main style={{ padding: "1rem" }}> */}
+                {/*             <p>There's nothing here!</p> */}
+                {/*         </main> */}
+                {/*     } */}
+                {/* ></Route> */}
+            </Routes>
+        </div>
+    )
 }
 
-export default App;
+function Editor() {
+    return (
+        <div>
+            bla
+        </div>
+    )
+}
+
+export default App
