81389eba93dcf5db9749a98bdf74b168a5d958be
piet <pprotassow@posteo.de>
2021-12-14 14:09:01 +0100

    done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
-import "./App.css";
+/** @format */
+
+import './App.css'
+import authors from './data'
 
 function App() {
-  return (
-    <div className="App flex flex-col items-center bg-gray-900 text-white h-full">
-       <p>Hello World</p>
-    </div>
-  );
+    return (
+        <div className="App flex flex-col items-center bg-gray-900 text-white h-full">
+            <ul>
+                {authors.map((x, i) => {
+                    return (
+                        <li key={i}>
+                            {x.name}:<ul>{x.books.map((y, ind) => {
+                                return (
+                                    <li key={ind}>{y}</li>    
+                                )
+                            })}</ul>
+                        </li>
+                    )
+                })}
+            </ul>
+        </div>
+    )
 }
 
-export default App;
+export default App
---
+++ src/data.js
@@ -0,0 +1,5 @@
+/** @format */
+
+const authors = [{ name: 'Mark Uwe KLing', books: ['kang1', 'kang2', 'kang3'] }, {name:'G.R.R. Martins', books:['GOT1','GOT2','GOT3','GOT4']}]
+
+export default authors
