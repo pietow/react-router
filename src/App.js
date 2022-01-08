1a94da09a2bc6a79dd7f922bfc7dcf3239c45ac9
piet <pprotassow@posteo.de>
2022-01-08 18:21:49 +0100

    react leaf

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import "./App.css";
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 
 function App() {
-  return (
-    <div className="App flex flex-col items-center bg-gray-900 text-white h-full">
-       <p>Hello World</p>
-    </div>
-  );
+    return (
+        <div>
+            <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+                <TileLayer
+                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+        <Marker position={[51.505, -0.09]}>
+            <Popup>
+                A pretty CSS3 popup. <br /> Easily customizable.
+            </Popup>
+        </Marker>
+        </MapContainer>
+        </div>
+    );
 }
 
 export default App;
